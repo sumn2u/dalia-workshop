a641253f24c392d66ac9df4f8c7083ce46619ff9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-31 02:34:49 +0000

    Deploy sumn2u/dalia-workshop to github.com/sumn2u/dalia-workshop.git:gh-pages

---
+++ precache-manifest.c72f9b173dc39b6360b788cdbe248eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e288457037ffa21c11",
+    "url": "/dalia-workshop/static/css/main.15ab7df0.chunk.css"
+  },
+  {
+    "revision": "90e288457037ffa21c11",
+    "url": "/dalia-workshop/static/js/main.90e28845.chunk.js"
+  },
+  {
+    "revision": "8e95736486ea008a363e",
+    "url": "/dalia-workshop/static/css/1.09c7ef43.chunk.css"
+  },
+  {
+    "revision": "8e95736486ea008a363e",
+    "url": "/dalia-workshop/static/js/1.8e957364.chunk.js"
+  },
+  {
+    "revision": "a26c5545bb6e014aef11",
+    "url": "/dalia-workshop/static/js/runtime~main.a26c5545.js"
+  },
+  {
+    "revision": "8887bfbdb4763c9197e2396a97c9dfc6",
+    "url": "/dalia-workshop/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/dalia-workshop/precache-manifest.c72f9b173dc39b6360b788cdbe248eba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dalia-workshop/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
